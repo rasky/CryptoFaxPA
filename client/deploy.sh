675ca702f383d899c94d275baf3443f958b44023
Alessandro Ranellucci <alessandro@pintle.it>
2018-10-03 21:10:21 +0200

    Add deploy script

---
+++ client/deploy.sh
@@ -0,0 +1,9 @@
+set -euo pipefail
+set -x
+
+GOOS=linux GOARCH=arm GOARM=7 go build
+ssh pi@cryptofaxpa.local rm realcrypto/client
+scp client pi@cryptofaxpa.local:/home/pi/realcrypto
+ssh pi@cryptofaxpa.local sudo killall client || true
+ssh pi@cryptofaxpa.local tail -f /var/log/syslog
+
