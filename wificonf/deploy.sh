26f9eb53229e413a015062fd17df710337a704f7
Giovanni Bajo <rasky@develer.com>
2018-10-10 02:56:45 +0200

    Fix configuration scripts to start wificonf when AP0 gets up

--- wificonf/deploy.sh
+++ wificonf/deploy.sh
@@ -3,5 +3,6 @@ set -euxo pipefail
 
 env GOOS=linux GOARCH=arm GOARM=7 packr build -ldflags="-s -w"
 ssh pi@cryptofaxpa.local killall wificonf || true
-scp wificonf pi@cryptofaxpa.local:
-ssh pi@cryptofaxpa.local ./wificonf -listen 0.0.0.0:8080
+ssh pi@cryptofaxpa.local rm ./realcrypto/wificonf || true
+scp wificonf pi@cryptofaxpa.local:realcrypto/
+ssh pi@cryptofaxpa.local ./realcrypto/wificonf -listen 0.0.0.0:8080
