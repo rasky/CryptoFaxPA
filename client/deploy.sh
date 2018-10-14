3c61dfacda6fe3a5e0dff519f94555859aa582a0
Giovanni Bajo <rasky@develer.com>
2018-10-14 23:25:08 +0200

    Change deploy script for client for new installation location

--- client/deploy.sh
+++ client/deploy.sh
@@ -3,8 +3,7 @@ set -euo pipefail
 set -x
 
 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-s -w"
-ssh pi@cryptofaxpa.local rm realcrypto/client
-scp client pi@cryptofaxpa.local:/home/pi/realcrypto
+ssh pi@cryptofaxpa.local rm client
+scp client pi@cryptofaxpa.local:
 ssh pi@cryptofaxpa.local sudo killall client || true
 ssh pi@cryptofaxpa.local tail -f /var/log/syslog
-
