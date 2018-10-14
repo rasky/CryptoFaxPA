7adafefffc3e4ca42ea09f04cbd65e1f1a3ddc6d
Giovanni Bajo <rasky@develer.com>
2018-10-14 19:45:10 +0200

    Improve error reporting

--- overlay/usr/local/sbin/swupdate.sh
+++ overlay/usr/local/sbin/swupdate.sh
@@ -6,18 +6,42 @@ ASSET_SWFAX="firmware.swfax"
 KEYRING="/home/pi/trusted.gpg"
 LAST_TIMESTAMP="/var/cache/firmware.last_updated"
 
-# Latest GitHub release ID
-RELID=$(curl -s $GHAPI/releases/latest | jq -r .id)
+# Do a http_get request to the specified URL.
+# When the HTTP result code is 2xx, outputs the body of the result on stdout.
+# When the HTTP result code is not 2xx (or the request fails at the HTTP-level
+# for other errors), an error message that includes the response body is
+# output to stderr, and the script is aborted.
+function http_get() {
+	TMP_CURL=$(mktemp "${TMPDIR:-/tmp/}$(basename "$0").XXXXXXXXXXXX")
+	trap 'rm -f "$TMP_CURL"' EXIT
+	HTTP_RES=$(curl -L -s \
+		--write-out "%{http_code}" \
+		-o "$TMP_CURL" \
+		"$1")
+	if [ "${HTTP_RES::1}" != "2" ]; then
+		>&2 echo "HTTP request failed: code = $HTTP_RES"
+		>&2 cat "$TMP_CURL"
+		exit 1
+	fi
+	cat "$TMP_CURL"
+	rm -f "$TMP_CURL"
+}
+
+# Fetch the latest GitHub release ID
+RELID=$(http_get $GHAPI/releases/latest | jq -r .id)
 if [ "$RELID" == "" ]; then
 	echo "No release found on GitHub in the cryptofax repo (?)"
 	exit 1
 fi
 
-ASSET=$(curl -s "$GHAPI/releases/$RELID/assets" | jq -r ".[] | select(.name==\"$ASSET_SWFAX\")")
+# Fetch the JSON description of the asset whose name is $ASSET_SWFAX.
+ASSET=$(http_get "$GHAPI/releases/$RELID/assets" | jq -r ".[] | select(.name==\"$ASSET_SWFAX\")")
 
+# Get the last-udpated timestamp and direct download URL of the asset
 UPDATED_AT=$(echo "$ASSET" | jq -r .updated_at)
 URL=$(echo "$ASSET" | jq -r .browser_download_url)
 
+# If the timestamp has not changed since the last update, exit without error
 touch $LAST_TIMESTAMP   # make sure it exists, even if empty
 if [ "$(cat $LAST_TIMESTAMP)" == "$UPDATED_AT" ]; then
 	echo "No new firmware found (last updated: $UPDATED_AT)"
@@ -27,7 +51,7 @@ fi
 trap 'rm -f "/tmp/$ASSET_SWFAX" "/tmp/$ASSET_SWFAX.tgz"' EXIT
 
 echo "Downloading new firmware: $URL"
-curl -L -s "$URL" >/tmp/$ASSET_SWFAX
+http_get "$URL" >/tmp/$ASSET_SWFAX
 
 echo "Verifying new firmware"
 if ! gpg --no-default-keyring --no-auto-key-retrieve \
