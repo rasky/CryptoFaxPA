20b11d8ba84c1cf05ef4d956e4a05aae19b41b1c
Alessandro Ranellucci <alessandro@pintle.it>
2018-10-15 20:14:23 +0200

    Improvements/updates to deploy scripts

--- client/deploy.sh
+++ client/deploy.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 set -x
 
 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-s -w"
-ssh pi@cryptofaxpa.local rm client
+ssh pi@cryptofaxpa.local rm client || true
 scp client pi@cryptofaxpa.local:
+scp *.ogg pi@cryptofaxpa.local:
 ssh pi@cryptofaxpa.local sudo killall client || true
 ssh pi@cryptofaxpa.local tail -f /var/log/syslog
