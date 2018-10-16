d1cd767b7b9c85d575237fc0d53cd24b5789745e
Giovanni Bajo <rasky@develer.com>
2018-10-16 12:05:58 +0200

    Run wificonf as root (for interface binding)

--- wificonf/deploy.sh
+++ wificonf/deploy.sh
@@ -5,4 +5,4 @@ env GOOS=linux GOARCH=arm GOARM=7 packr build -ldflags="-s -w"
 ssh pi@cryptofaxpa.local killall wificonf || true
 ssh pi@cryptofaxpa.local rm ./wificonf || true
 scp wificonf pi@cryptofaxpa.local:
-ssh pi@cryptofaxpa.local ./wificonf -listen 0.0.0.0:8080
+ssh pi@cryptofaxpa.local sudo ./wificonf -listen 0.0.0.0:8080
